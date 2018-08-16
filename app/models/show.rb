657bb369ad7d1d955f06b0314979337e97e6e1b2
mguevara717 <marissa.guevara717@gmail.com>
2018-08-16 17:18:25 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,38 @@
+require "pry"
+class Show < ActiveRecord::Base
+
+def self.highest_rating
+    self.maximum('rating')
+end
+
+def self.most_popular_show
+  self.all.find do |show|
+      show.rating == self.highest_rating #iterate through using find to compare
+  end
+end
+
+def self.lowest_rating
+  self.minimum('rating')
+end
+
+
+def self.least_popular_show
+  self.all.find do |show|
+    show.rating == self.lowest_rating
+  end
+end
+
+def self.ratings_sum
+    self.sum('rating')
+
+end
+
+def self.popular_shows
+  self.where('rating > ?', 5)
+end
+
+def self.shows_by_alphabetical_order
+    self.order('name')
+end
+
+end #end of class
